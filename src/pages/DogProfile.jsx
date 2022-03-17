416773cb511b2cc0a5a49fc906c6407f2950378a
Ana Iacovache <eaaania@students.eaaa.dk>
2022-03-17 14:18:11 +0100

    dog profile progress

---
+++ src/pages/DogProfile.jsx
@@ -0,0 +1,73 @@
+import {
+  IonCard,
+  IonCardContent,
+  IonCardSubtitle,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonPage,
+  IonIcon,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./DogProfile.css";
+import { addCircleOutline } from "ionicons/icons";
+import dogExamplePhoto from "../images/dog-example-photo.png";
+
+const DogProfile = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Profile</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Profile</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonCard className="user-container">
+          <IonCardContent className="user-info-section">
+            <div className="user-image-container">
+              <IonImg
+                src={dogExamplePhoto}
+                className="user-profile-photo"
+                alt="user"
+              />
+              <p>Edit</p>
+            </div>
+
+            <div className="user-info-text">
+              <IonCardSubtitle>Jens Frederiken</IonCardSubtitle>
+              <p>Tordenskjoldgade 2, 3.7, Aarhus C</p>
+            </div>
+          </IonCardContent>
+        </IonCard>
+        <h2 className="dogs-container-title">My Dog(s)</h2>
+        <ul className="dogs-container">
+          <li className="dog-container">
+            <IonImg
+              src={dogExamplePhoto}
+              className="dog-profile-photo"
+              alt="dog"
+            />
+            <p className="dog-name">Amazing</p>
+          </li>
+          <li className="dog-container">
+            <IonImg
+              src={dogExamplePhoto}
+              className="dog-profile-photo"
+              alt="dog"
+            />
+            <p className="dog-name">Amazing</p>
+          </li>
+          <IonIcon icon={addCircleOutline} className="add-icon" />
+        </ul>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default DogProfile;
